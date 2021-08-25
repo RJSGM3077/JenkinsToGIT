52cc2f902d64719aa8023d17645155b42aa2665f
RJSGM3077 <88161351+RJSGM3077@users.noreply.github.com>
2021-08-24 22:40:40 -0400

    Update OpenBing.java

--- OpenBing.java
+++ OpenBing.java
@@ -1,6 +1,6 @@
 public class OpenBing {
 	public static void main(String[] args)
 	{
-	System.out.println(" I am opening Bing for Jenkins4");
+	System.out.println(" I am opening Bing for Jenkins");
 	}
 }
